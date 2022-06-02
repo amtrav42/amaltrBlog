a283dad93e9f6fd6e637bc721a6d3226af002375
Amelia <amelia.travers@gmail.com>
2022-06-02 22:39:02 +1000

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,7 @@ import prismicLogo from '../images/logo-prismic.svg'
 export const Footer = () => (
   <footer className="container">
     <p>
-      Proudly published with{' '}
-      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-        Prismic
-      </a>
-      <br />
-      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-        <img
-          className="footer-logo"
-          src={prismicLogo}
-          alt="Gray Prismic logo"
-        />
-      </a>
+    I would like to acknowledge the Traditional Custodians of the lands on which this website was created, is hosted and is viewed. The website is created on the lands of the Wautharong peoples of the Kulin Nation. I pay my respects to their Elders past and present, and acknowledge them as the original inspiring and ongoing contributors to STEM and storytelling in Australia.
     </p>
   </footer>
 )
